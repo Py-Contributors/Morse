8bbfc718fd418cc9a81099f22f2b0d52f85beff5
Deepak Raj <54245038+codePerfectPlus@users.noreply.github.com>
2024-06-08 13:47:38 +0530

    chore: Add test.py to .gitignore and update requirements.txt

--- morse/main.py
+++ morse/main.py
@@ -1,68 +1,26 @@
-import sys
-sys.path.append('.')
 from morse.utils import load_config
-
-morse_mapping = load_config('morse_mapping.json')
-
-alpha_to_morse_mapping = morse_mapping['alpha_to_morse_mapping']
-number_to_morse_mapping = morse_mapping['number_to_morse_mapping']
-morse_to_alpha_mapping = {v: k for k, v in alpha_to_morse_mapping.items()}
-morse_to_number_mapping = {v: k for k, v in number_to_morse_mapping.items()}
+morse_mapping_json = load_config('morse_mapping.json')
 
 
 class Morse(object):
     def __init__(self):
-        pass
+        self.character_mapping = {**morse_mapping_json['morse_mapping'], **{k.lower(): v for k, v in \
+                                morse_mapping_json['morse_mapping'].items()}}
+        self.reverse_character_mapping = {v: k for k, v in self.character_mapping.items()}
+        self.binary_mapping = morse_mapping_json['binary_mapping']
+        self.reverse_binary_mapping = {v: k for k, v in self.binary_mapping.items()}
 
     def binary_to_morse(self, binary):
-        morse_code = []
-        for char in binary:
-            if char == '1':
-                morse_code.append('-')
-            elif char == '0':
-                morse_code.append('.')
-        return ''.join(morse_code)
+        binary_string = str(binary)
+        return "".join([self.binary_mapping[char] for char in binary_string])
 
     def morse_to_binary(self, morse_code):
-        binary = []
-        for char in morse_code:
-            if char == '-':
-                binary.append('1')
-            elif char == '.':
-                binary.append('0')
-        return ''.join(binary)
+        return "".join([self.reverse_binary_mapping[char] for char in morse_code])
 
     def encode_morse(self, text):
-        morse_code = []
-        for char in text:
-            if char.isalpha():
-                morse_code.append(alpha_to_morse_mapping[char.upper()])
-            elif char.isdigit():
-                morse_code.append(number_to_morse_mapping[char])
-            else:
-                morse_code.append(char)
-        return ' '.join(morse_code)
+        return ' '.join([self.character_mapping[char] if char in self.character_mapping \
+                         else char for char in text])
 
     def decode_morse(self, morse_code):
-        text = []
-        for char in morse_code.split(' '):
-            if char in morse_to_alpha_mapping:
-                text.append(morse_to_alpha_mapping[char])
-            elif char in morse_to_number_mapping:
-                text.append(morse_to_number_mapping[char])
-            else:
-                text.append(char)
-        return ''.join(text)
-
-    def encode_morse_to_audio(self, text, output_file_path):
-        pass
-        # morse_code = self.encode_morse(text)
-        # for char in morse_code:
-        #     if char == '.':
-        #         playsound('src/sounds/dit.wav')
-        #     elif char == '-':
-        #         playsound('src/sounds/dah.wav')
-        #     elif char == ' ':
-        #         playsound('src/sounds/short_pause.wav')
-        #     else:
-        #         playsound('src/sounds/long_pause.wav')
+        return ''.join([self.reverse_character_mapping[char].lower() if char in self.reverse_character_mapping \
+                        else char for char in morse_code.split()])
--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import setuptools
+from glob import glob
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -6,21 +7,31 @@ with open("README.md", "r") as fh:
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
+__name__ = "Morse"
+__version__ = "1.0.0"
+__author__ = "Deepak Raj"
+__author_email__ = "deepak008@live.com"
+__description__ = "A package to convert text to morse code and vice versa"
+__documentation__ = "https://pycontributors.readthedocs.io/projects/morse/en/latest/"
+__source__ = "https://github.com/Py-Contributors/morse"
+__tracker__ = "https://github.com/Py-Contributors/morse/issues"
+
 setuptools.setup(
-    name="Morse",
-    version="1.0.0",
-    author="CodePerfectPlus",
-    author_email="deepak008@live.com",
-    description="A package to convert text to morse code and vice versa",
+    name=__name__,
+    version=__version__,
+    author=__author__,
+    author_email=__author_email__,
+    description=__description__,
     long_description=long_description,
     long_description_content_type="text/markdown",
-    install_requires=required,
+    data_files=[('assets', glob('morse/assets/*'))], 
+    include_package_data=True,
     url="https://github.com/codePerfectPlus/morse",
-    keywords="audiobook",
+    keywords="morse code, text to morse, morse to text",
     packages=setuptools.find_packages(),
-    project_urls={"Documentation": "https://pycontributors.readthedocs.io/projects/morse/en/latest/",
-                  "Source": "https://github.com/Py-Contributors/morse",
-                  "Tracker": "https://github.com/Py-Contributors/morse/issues"},
+    project_urls={"Documentation": __documentation__,
+                  "Source": __source__,
+                  "Tracker": __tracker__},
     classifiers=["Development Status :: 2 - Pre-Alpha",
                  "Programming Language :: Python :: 3",
                  "License :: OSI Approved :: MIT License",
